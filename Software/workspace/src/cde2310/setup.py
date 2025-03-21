5bfea22e448b71934cd89b148da8d855d0d5a120
HarshitSrivastavaHS <harshitsrivastava7149@gmail.com>
2025-03-21 13:08:12 +0800

    Automatic exploration of the map

--- Software/workspace/src/cde2310/setup.py
+++ Software/workspace/src/cde2310/setup.py
@@ -1,25 +1,28 @@
-from setuptools import find_packages, setup
+from setuptools import setup
+import os
+from glob import glob
 
 package_name = 'cde2310'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude=['test']),
-    data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-    ],
+    packages=[package_name],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='harshit',
-    maintainer_email='harshitsrivastava7149@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    maintainer='Your Name',
+    maintainer_email='your_email@example.com',
+    description='Custom bringup package for robot autonomy',
+    license='Apache License 2.0',
     tests_require=['pytest'],
     entry_points={
-        'console_scripts': [
-        ],
+        'console_scripts': [],
     },
+    data_files=[
+        # Install launch files
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+        # Install configuration files
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
+    ],
 )
+
